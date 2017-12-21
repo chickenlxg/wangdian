77c20792b3181a77438655932e4c915c81fc2267
chiceknlxg <1337695331@qq.com>
2017-12-21 17:55:11 +0800

    分类页面

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,7 +1,36 @@
 var app = getApp();
+import menus from './menus.js'  //添加
 Page({
   data: {
-    categories: []
+    categories: [],
+    //添加
+    text: "Page main",
+    background: [
+      {
+        color: 'green',
+        sort: 1
+      },
+      {
+        color: 'red',
+        sort: 2
+      },
+      {
+        color: 'yellow',
+        sort: 3
+      }
+    ],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 3000,
+    duration: 1200,
+    toView: 'aa',
+    selectedMenuId: 1,
+    total: {
+      count: 0,
+      money: 0
+    }
+    //添加
   },
   navigateToCategoryProduct(event) {
     var categoryId = event.currentTarget.dataset.cateId;
@@ -10,15 +39,59 @@ Page({
       url: '../category-product/category-product?id=' + categoryId + '&title=' + activityTitle,
     })
   },
+  selectMenu: function (event) {
+    let data = event.currentTarget.dataset
+    this.setData({
+      toView: data.tag,
+      selectedMenuId: data.id
+    })
+    // this.data.toView = 'red'
+  },
+  addCount: function (event) {
+    let data = event.currentTarget.dataset
+    let total = this.data.total
+    let menus = this.data.categories
+    console.log(menus);
+
+    dish.count += 1;
+    total.count += 1
+    total.money += dish.price
+    this.setData({
+      'menus': menus,
+      'total': total
+    })
+  },
+  minusCount: function (event) {
+    let data = event.currentTarget.dataset
+    let total = this.data.total
+    let menus = this.data.menus
+    let menu = menus.find(function (v) {
+      return v.id == data.cid
+    })
+    let dish = menu.dishs.find(function (v) {
+      return v.id == data.id
+    })
+    if (dish.count <= 0)
+      return
+    dish.count -= 1;
+    total.count -= 1
+    total.money -= dish.price
+    this.setData({
+      'menus': menus,
+      'total': total
+    })
+  },
   onLoad() {
     var that = this;
     wx.request({
-      url: app.serverURL + '/get/web/activity.php', 
+      url: app.serverURL + '/get/web/cateActivity.php', 
       header: {
         'content-type': 'application/json'
       },
+      data: {
+        userID: app.globalData.userID
+      },
       success: function (res) {
-        console.log(res);
         that.setData({
           categories: res.data
         })
